0a82d25e63e3e7794ea1363b72f4fcf0eba8f906
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-03-12 00:13:34 +0300

    UI was updated.

--- app/src/main/java/com/metehanbolat/alarmmanagerwithnotificationcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/alarmmanagerwithnotificationcompose/MainActivity.kt
@@ -1,8 +1,23 @@
 package com.metehanbolat.alarmmanagerwithnotificationcompose
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.metehanbolat.alarmmanagerwithnotificationcompose.ui.theme.AlarmManagerWithNotificationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,8 +25,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AlarmManagerWithNotificationComposeTheme {
-
+                Surface(color = MaterialTheme.colors.background) {
+                    val context = LocalContext.current
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(
+                                        text = "AlarmManager Notification",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            )
+                        }
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "Set Alarm Time : 10 Seconds",
+                                modifier = Modifier
+                                    .padding(10.dp),
+                                fontSize = 16.sp
+                            )
+                            Button(
+                                onClick = {
+                                    setAlarm(context)
+                                }
+                            ) {
+                                Text(text = "Set Alarm")
+                            }
+                        }
+                    }
+                }
             }
         }
+    }
+
+    private fun setAlarm(context: Context) {
+        val timeSec = System.currentTimeMillis() + 10000
+        val alarmManager = context.getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, MyAlarm::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+        alarmManager.set(AlarmManager.RTC_WAKEUP, timeSec, pendingIntent)
     }
 }
